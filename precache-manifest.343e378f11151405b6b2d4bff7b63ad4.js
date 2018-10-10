075a17ed31a2c097892e4353eaad8996fce3b73d
ddexta <18assdd@gmail.com>
2018-10-10 19:38:01 +0100

    Updates

---
+++ precache-manifest.343e378f11151405b6b2d4bff7b63ad4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f88d103c25e2f71ccdbd",
+    "url": "/cmnored/static/js/runtime~main.f88d103c.js"
+  },
+  {
+    "revision": "793b87d3fe6631a9f253",
+    "url": "/cmnored/static/js/main.793b87d3.chunk.js"
+  },
+  {
+    "revision": "14bebbd5b9e58e731585",
+    "url": "/cmnored/static/js/1.14bebbd5.chunk.js"
+  },
+  {
+    "revision": "14bebbd5b9e58e731585",
+    "url": "/cmnored/static/css/1.f9a2727e.chunk.css"
+  },
+  {
+    "revision": "be45721204f736424f4cd15482dcda12",
+    "url": "/cmnored/index.html"
+  }
+];
